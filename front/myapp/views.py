3a4db603bc2e3ca864d9d64d2c7611a43445ca26
Daniilas Ivanovas <daniilas@Daniilass-MacBook-Pro.local>
2025-10-19 21:37:51 +0300

    pridejau login in register pages front ende

--- front/myapp/views.py
+++ front/myapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home_view(request):
+    return render(request, 'home.html')
+
+def login_view(request):
+    return render(request, 'login.html')
+
+def register_view(request):
+    return render(request, 'register.html')
