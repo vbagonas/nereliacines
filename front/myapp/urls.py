1c338d403adea8ad2ad6bb5b0607b3528fb01ba7
Daniilas Ivanovas <daniilas@Daniilass-MacBook-Pro.local>
2025-11-08 14:20:04 +0200

    sukurtas questions failas ir prideta i views

--- front/myapp/urls.py
+++ front/myapp/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
     path('home/', views.home_view, name='home'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    path('questions/', views.questions_view, name='questions')
 ]
--- front/myapp/views.py
+++ front/myapp/views.py
@@ -8,3 +8,6 @@ def login_view(request):
 
 def register_view(request):
     return render(request, 'register.html')
+
+def questions_view(request):
+    return render(request, 'questions.html')
