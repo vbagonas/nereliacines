ef712d0a208d111626cb21a9194f2621d727c1f4
Daniilas Ivanovas <daniilas@Daniilass-MacBook-Pro.local>
2025-11-30 23:34:43 +0200

    hotfix, sutvarkiau home.html ir dabar top3 logika yra handlinama app/routes/recommendations.py

--- backend/app/routes/recommendations.py
+++ backend/app/routes/recommendations.py
@@ -1,17 +1,19 @@
 from flask import Blueprint, jsonify
 from backend.app.extensions import neo4
-
+from backend.app.routes.analytics import top3_by_tickets as top3
 recommendations_bp = Blueprint('recommendations', __name__, url_prefix='/api/v1')
 
 
 @recommendations_bp.get("/recommendations/<user_id>")
 def get_recommendations(user_id):
     """Get all recommended events for user (riboto gylio)"""
+    print(f"Getting recommendations for user {user_id}")
     if neo4.has_purchase_history(user_id):
+        print("User has purchase history, getting collaborative recommendations")
         events = neo4.recommend_collaborative(user_id)
     else:
-        # be istorijos – tegul frontend rodo Top3
-        events = []
+        print("User has no purchase history, returning top3 events by tickets sold")
+        events = top3()
 
     return jsonify(events), 200
 
@@ -25,13 +27,15 @@ def get_upcoming_recommendations(user_id):
     jeigu nieko nėra – imam paprastus artimiausius.
     Jei istorijos nėra – irgi imam artimiausius.
     """
+    print(f"Getting upcoming recommendations for user {user_id}")
     if neo4.has_purchase_history(user_id):
+        print("User has purchase history, getting collaborative recommendations")
         events = neo4.recommend_collaborative_upcoming(user_id)
         if not events:
             events = neo4.get_upcoming_events(months=24, limit=5)
     else:
-        events = neo4.get_upcoming_events(months=24, limit=5)
-
+        print("User has no purchase history, returning top3 events by tickets sold")
+        events = top3()
     return jsonify(events), 200
 
 
