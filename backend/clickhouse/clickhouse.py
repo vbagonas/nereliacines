5ac84b3e2e4ab31921725120845c75dce1321ad8
Daniilas Ivanovas <daniilas@Daniilass-MacBook-Pro.local>
2025-12-12 12:59:15 +0200

    added user auth

--- backend/clickhouse/clickhouse.py
+++ backend/clickhouse/clickhouse.py
@@ -1,14 +1,21 @@
 import clickhouse_connect
+from pathlib import Path
+import yaml 
 
+PROJECT_ROOT = Path(__file__).resolve().parent.parent.parent
 class ClickHouseClient():
     def __init__(self) -> None:
         self.client = self.connect()
 
     def connect(self):
+        with open(PROJECT_ROOT / 'creds.yml', 'r') as f:
+            data = yaml.safe_load(f) or {}
+        creds = data.get('neo4j_user', data)
+
         client = clickhouse_connect.get_client(
         host='e4h24gda5p.eu-central-1.aws.clickhouse.cloud',
-        user='default',
-        password='<password>',
+        user=creds['username'],
+        password=creds['password'],
         secure=True
         )
 
